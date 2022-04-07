028633cce57922ebd5a216079157d81b52140388
Jeison <jeiison.rodriguez7@gmail.com>
2022-04-07 16:22:34 -0500

    primer commit

---
+++ pages/api/categorias.js
@@ -0,0 +1,13 @@
+import {PrismaClient} from '@prisma/client'
+
+export default async function handler( req, res ) {
+  
+  const prisma = new PrismaClient()
+  const categorias = await prisma.categoria.findMany({
+    include: {
+      productos: true
+    }
+  })
+  return res.status(200).json(categorias);
+}
+
